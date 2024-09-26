a73a5b32d0e08ab74142b6744c6e42896d401bac
TannerHornsby7 <tannerhornsby386@gmail.com>
2024-09-26 12:34:45 -0500

    use env vars for stack config

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 #!/.venv/bin/python
 from aws_cdk import App 
-from lib.server import DiscoCraftainer
+from aws_cdk import Environment
+from lib.server import Discocraftainer
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+# set up account and region for the app
+account=os.environ["AWS_ACCOUNT_ID"]
+region=os.environ["AWS_REGION"]
 
 app = App()
-DiscoCraftainer(app, "Discocraftainer")
+Discocraftainer(app, "Discocraftainer", env=Environment(account=account, region=region))
 
 app.synth()
