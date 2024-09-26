f3d8b5dfbd4c686ac04bcd610ec1f6bc13009771
TannerHornsby7 <tannerhornsby386@gmail.com>
2024-09-26 12:35:05 -0500

    place fargate service behind a nlb, point arecord to the alb ip

--- lib/platform.py
+++ lib/platform.py
@@ -1,23 +1,36 @@
 from aws_cdk import (
     aws_ecs as ecs,
     aws_ec2 as ec2,
-    Stack,  
+    Stack,
+    Environment
 )
 from constructs import Construct
 
-class DiscoCraftainerPlatform(Stack):
-    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
-        # Define your VPC
-        self.vpc = ec2.Vpc(self, "DiscoCraftainerVPC",
-                      vpc_name="DiscoCraftainerVPC",
-                      max_azs=3, # Default is all AZs in the region
-                      nat_gateways=0,
-                      )
-        # Define your ECS Cluster
-        self.cluster = ecs.Cluster(self, "DiscoCraftainerCluster",
-                            cluster_name="DiscoCraftainerCluster",
-                            vpc=self.vpc,
-                            # container_insights=True
-                            )
-        # Add other resources and configurations here
+class DiscocraftainerPlatform(Stack):
+    def __init__(self, scope: Construct, id: str, env: Environment, **kwargs) -> None:
+        super().__init__(scope, id, env=env, **kwargs)
+
+        # Create a VPC for the service
+        self.vpc = ec2.Vpc(
+            self, "DiscocraftainerVPC",
+            vpc_name="DiscocraftainerVPC",
+            max_azs=3,
+            nat_gateways=0,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="public",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24
+                )
+            ],
+            enable_dns_hostnames=True,
+            enable_dns_support=True
+        )
+        
+        # create a cluster for the service
+        self.cluster = ecs.Cluster(self, "DiscocraftainerCluster",
+            vpc = self.vpc,
+            cluster_name="DiscocraftainerCluster",
+            container_insights=True
+        )
+        
--- lib/server.py
+++ lib/server.py
@@ -1,17 +1,28 @@
-from .platform import DiscoCraftainerPlatform
+from .platform import DiscocraftainerPlatform
 from constructs import Construct
 from aws_cdk import (
     aws_ecs as ecs,
     aws_iam as iam,
-    Stack,
     aws_ec2 as ec2,
+    aws_ecs_patterns as ecs_patterns,
+    aws_route53 as route53,
+    Stack,
+    Environment,
+    Duration,
+    aws_elasticloadbalancingv2 as elbv2
 )
+import os
+import dotenv
 
-class DiscoCraftainer(Stack):
-    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
+dotenv.load_dotenv()
+
+server_port = int(os.environ['SERVER_PORT'])
+
+class Discocraftainer(Stack):
+    def __init__(self, scope: Construct, id: str, env: Environment, **kwargs) -> None:
+        super().__init__(scope, id, env=env, **kwargs)
         
-        self.platform = DiscoCraftainerPlatform(self, "DiscoCraftainerPlatform")
+        self.platform = DiscocraftainerPlatform(self, "DiscocraftainerPlatform", env=env)
         
         # create a role for the task to use for logging
         self.task_role = iam.Role(
@@ -33,12 +44,12 @@ class DiscoCraftainer(Stack):
         # now we will define the ECS fargate service which will run both our
         # minecraft server and the watchdog container
         self.task_definition = ecs.FargateTaskDefinition(
-            self, "DiscoCraftainerTaskDef",
+            self, "DiscocraftainerTaskDef",
             cpu=1024,
             memory_limit_mib=2048,
             task_role=self.task_role,
-            execution_role=self.task_role
-        )       
+            execution_role=self.task_role,
+        )
          
         
         # add the minecraft container to the task definition
@@ -46,7 +57,7 @@ class DiscoCraftainer(Stack):
             "MinecraftContainer",
             image=ecs.ContainerImage.from_registry("itzg/minecraft-server"),
             environment={
-                "EULA": "TRUE"
+                "EULA": "TRUE",
             },
             logging=ecs.LogDrivers.aws_logs(
                 stream_prefix="Minecraft"
@@ -54,35 +65,47 @@ class DiscoCraftainer(Stack):
         )
         
         self.minecraft_container.add_port_mappings(
-            ecs.PortMapping(container_port=25565, host_port=25565, protocol=ecs.Protocol.TCP)
+            ecs.PortMapping(container_port=server_port, protocol=ecs.Protocol.TCP)
         )
         
-        # # now we will add a watchdog container to the task defintion
-        # # this container is responsible for monitoring the minecraft server
-        # # and when it has been idle for config.MINECRAFT_IDLE_TIMEOUT seconds
-        # # it will stop the server (set the desired task count to 0)
-        # self.watchdog_container = self.task_definition.add_container(
-        #     "WatchdogContainer",
-        #     image=ecs.ContainerImage.from_asset("watchdog"),
-        #     environment={
-        #         "MINECRAFT_IDLE_TIMEOUT": str(config.MINECRAFT_IDLE_TIMEOUT),
-        #         "CLUSTER": self.platform.cluster.cluster_name,
-        #         "SERVICE": self.service.service_name
-        #     },
-        #     logging=ecs.LogDrivers.aws_logs(
-        #         stream_prefix="Watchdog"
-        #     )
-        # )
+        # configure the subdomain
+        self.hosted_zone = route53.HostedZone.from_lookup(
+            self, "OuradioHostedZone",
+            domain_name="ouradio.net"
+        )
+        
+        # create the service security group
+        self.service_sg = ec2.SecurityGroup(
+            self, "DiscocraftainerServiceSecurityGroup",
+            vpc=self.platform.vpc,
+            allow_all_outbound=True
+        )
+        
+        self.service_sg.add_ingress_rule(
+            ec2.Peer.any_ipv4(),
+            ec2.Port.tcp(server_port),
+            "Allow TCP on port " + str(server_port)
+        )
         
         # now lets define the fargate service which will run our task definition
-        self.service = ecs.FargateService(
-            self, "DiscoCraftainerService",
+        self.service = ecs_patterns.NetworkLoadBalancedFargateService(
+            self, "DiscocraftainerService",
             cluster=self.platform.cluster,
             task_definition=self.task_definition,
             desired_count=1,
-            assign_public_ip=True
-        )
+            domain_name="minecraft.ouradio.net",
+            domain_zone=self.hosted_zone,
+            listener_port=int(server_port),
+            public_load_balancer=True,
+            assign_public_ip=True,
+            security_groups=[self.service_sg],
+            )
         
-        self.service.connections.allow_from_any_ipv4(
-            ec2.Port.tcp(25565)
+        # configure the health check for the target group
+        self.service.target_group.configure_health_check(
+            port=str(server_port),
+            protocol=elbv2.Protocol.TCP,
+            healthy_threshold_count=2,
+            unhealthy_threshold_count=2,
+            timeout=Duration.seconds(10)
         )
